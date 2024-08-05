2c25cb622b16bb5c7b82b794458b1d92512410ea
Bashikdev <bashikdev@gmail.com>
2024-08-06 00:04:55 +0300

    init commit

---
+++ Lessons/lesson_1_var_operations/homework_1/concatenation/main.js
@@ -0,0 +1,9 @@
+let firstName = 'Dmytro';
+let middleName = 'Bashynskyi';
+let lastName = 'Stanislavovych';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+let person2 = `${firstName} ${middleName} ${lastName}`;
+console.log(person2);
---
+++ Lessons/lesson_1_var_operations/homework_1/prompt/main.js
@@ -0,0 +1,9 @@
+let firstName = prompt("Введіть ваше ім'я:");
+
+let surname = prompt("Введіть ваше по батькові:");
+
+let age = prompt("Введіть ваш вік:");
+
+console.log(`Ім'я: ${firstName}`);
+console.log(`По батькові: ${surname}`);
+console.log(`Вік: ${age}`);
