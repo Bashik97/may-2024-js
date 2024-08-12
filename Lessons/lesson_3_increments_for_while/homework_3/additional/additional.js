72827ede38463feee8e5569ece5c63a42313dcf3
Bashikdev <bashikdev@gmail.com>
2024-08-12 15:31:45 +0300

    additional

---
+++ Lessons/lesson_3_increments_for_while/homework_3/additional/additional.js
@@ -0,0 +1,17 @@
+//           1              //
+let numberArray = [1, 2, 3, 17, -54];
+let stringArray = ['apple', 'banana', 'cherry', 'dog'];
+let mixedArray = ['bye', 35, true, -41, false];
+console.log(numberArray);
+console.log(stringArray);
+console.log(mixedArray);
+
+//           2              //
+let emptyArray = [];
+emptyArray[0] = 1;
+emptyArray[1] = true;
+emptyArray[2] = 'hello';
+emptyArray[3] = true;
+emptyArray[4] = -45;
+emptyArray[5] = false;
+console.log(emptyArray);
