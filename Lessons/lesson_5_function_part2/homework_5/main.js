cfaabcfe1ca0939b960f747d1a9094b867965f82
Bashikdev <bashikdev@gmail.com>
2024-08-15 14:30:13 +0300

    homework5

--- Lessons/lesson_5_function_part2/homework_5/main.js
+++ Lessons/lesson_5_function_part2/homework_5/main.js
@@ -1,53 +1,38 @@
-//                           1                         //
-function calculateRectangleArea(a, b) {
-    return a * b;
-}
+//                         1                         //
+let calculateRectangleArea = (a, b) => a * b;
+console.log(calculateRectangleArea(5, 9));
 
-let example1 = calculateRectangleArea(5, 13);
-console.log(example1);
+//                         2                         //
+let calculateCircleArea = (radius) => Math.PI * radius * radius;
+console.log(calculateCircleArea(7));
 
-//                           2                         //
-function calculateCircleArea(radius) {
-    return Math.PI * radius * radius;
-}
+//                         3                         //
+let calculateCylinderArea = (height, radius) => 2 * Math.PI * radius * radius + 2 * Math.PI * radius * height;
+console.log(calculateCylinderArea(9, 16));
 
-let example2 = calculateCircleArea(5);
-console.log(example2);
-
-//                           3                          //
-// S(повн.)=2S(осн.)+S(біч.)= 2πR2 + 2πRH
-function calculateCylinderArea(height, radius) {
-    return 2 * Math.PI * radius * radius + 2 * Math.PI * radius * height;
-}
-
-let example3 = calculateCylinderArea(8, 4);
-console.log(example3);
-
-//                           4                          //
-function printArrayElements(arr) {
+//                         4                         //
+let printArrayElements = (arr) => {
     for (let item of arr) {
-        console.log(item);
+    console.log(item);
     }
-}
+};
+printArrayElements([5, false, -45, 'true', true, 'hello'])
 
-let example4 = [5, false, -45, true, 'hello'];
-printArrayElements(example4);
-
-//                           5                          //
-function createParagraph(text) {
+//                         5                         //
+let createParagraph = (text) => {
     document.write(
         `
         <div>
             <p>${text}</p>
         </div>
         `
-    );
-}
+    )
+};
 
 createParagraph('Lorem ipsum dolor sit amet.');
 
 //                           6                          //
-function createListWithItems(text) {
+let createListWithItems = (text) => {
     document.write(
         `
         <ul>
@@ -57,38 +42,38 @@ function createListWithItems(text) {
         </ul>
         `
     )
-}
+};
 
 createListWithItems('hello friends');
 
 //                           7                          //
-function list(text, counter) {
+let list = (text, counter) => {
     document.write('<ul>');
     for (let i = 0; i < counter; i++) {
         document.write(`<li>${text}</li>`)
     }
     document.write('</ul>');
-}
+};
 
 list('hello', 10);
 
 //                           8                          //
-function foobar(arrayOfPrimitives) {
+let foobar = (arrayOfPrimitives) => {
     document.write('<ul>');
     for (const item of arrayOfPrimitives) {
         document.write(`<li>${item}</li>`);
     }
     document.write('</ul>');
-}
+};
 
 foobar(['hello', 125, true, -568, false, 4]);
 
 //                           9                          //
-function foobar2(users) {
+let foobar2 = (users) => {
     for (const user of users) {
         document.write(`<div>${user.id} ${user.name} ${user.age}</div>`);
     }
-}
+};
 
 foobar2([
     {id: 1, name: 'kolya', age: 25},
@@ -96,10 +81,10 @@ foobar2([
     {id: 3, name: 'petya', age: 32},
     {id: 4, name: 'maxym', age: 21},
     {id: 5, name: 'igor', age: 33},
-])
+]);
 
 //                           10                          //
-function minValue(numbers) {
+let minValue = (numbers) => {
     let min = numbers[0];
     for (let i = 1; i < numbers.length; i++) {
         let number = numbers[i];
@@ -108,18 +93,18 @@ function minValue(numbers) {
         }
     }
     return min;
-}
+};
 
 console.log(minValue([12, 452, 75, -41, 27, 3, -759, 16]));
 
 //                           11                          //
-function sum(arr) {
+let sum = (arr) => {
     let total = 0;
     for (const item of arr) {
         total = total + item;
     }
     return total;
-}
+};
 
 console.log(sum([1, 2, 3, 4, 5]));
 console.log(sum([5, 2, 9, 24, 3]));
@@ -127,20 +112,12 @@ console.log(sum([-4, 8, 69, -42, 15]));
 
 //                           12                          //
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-function swap(arr, index1, index2) {
+let swap = (arr, index1, index2) => {
     let cell = arr[index1]; // 11
     arr[index1] = arr[index2]; // 11 => 22
     arr[index2] = cell; // 22 => 11
 
     return arr;
-}
+};
 
 console.log(swap([11, 22, 33, 44], 0, 1));
-
-//                           13                          //
-// Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
-// Приклад exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD') // => 400
-
-function exchange(sumUAH, currencyValues, exchangeCurrency) {
-
-}
